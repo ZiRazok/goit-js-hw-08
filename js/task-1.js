38697cbb60a70f16a6b7b09cf9d9eb2e7a037cb1
ZiRazok <45107612+ZiRazok@users.noreply.github.com>
2024-05-12 03:18:55 +0300

    HW-JS-08

--- js/task-1.js
+++
@@ -1,6 +0,0 @@
-const categories = document.querySelectorAll(".item");
-console.log(`Number of categories: ${categories.length}`);
-categories.forEach(element => {
-    console.log(`Category: ${element.querySelector("h2").innerText}`);
-    console.log(`Elements: ${element.querySelector("ul").querySelectorAll("li").length}`)
-});
--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const textInput = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-textInput.addEventListener("input", event => {
-    event.currentTarget.value = event.currentTarget.value.trim();
-    event.currentTarget.value ? output.textContent = event.currentTarget.value : output.textContent = "Anonymous";
-});
